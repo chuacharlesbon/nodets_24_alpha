7b0eda7c3c6abc9176433aafa8592624028880ec
Charles Bon Chua <chuacharlesbon@gmail.com>
2026-02-22 21:52:58 +0800

    Updated: Restructured App Routes

--- src/routes/mobAppRouter.ts
+++ src/routes/mobAppRouter.ts
@@ -1,18 +1,12 @@
 import express, { Router } from "express";
-import { checkBackUpShellController, shellController1 } from "../controllers/shellController";
-import { logsViewController, logsErrTestController, rateLimitLogsViewController } from "../controllers/logController";
+import shellRouter from "./shellRouter";
 
 const mobAppRouter: Router = express.Router();
+mobAppRouter.use("/shell", shellRouter);
 
 mobAppRouter.get("/ping", (req, res) => {
     console.log("Request Method", req.method);
     res.status(200).json({message: "Connected to mobile app route."});
 });
-
-mobAppRouter.get("/shell-test", shellController1);
-mobAppRouter.get("/shell-backup-check", checkBackUpShellController);
-mobAppRouter.get("/logs-view", logsViewController);
-mobAppRouter.get("/rate-limit-logs-view", rateLimitLogsViewController);
-mobAppRouter.get("/logs-err-test", logsErrTestController);
 
 export default mobAppRouter;
--- src/routes/webRouter.ts
+++ src/routes/webRouter.ts
@@ -1,18 +1,12 @@
 import express, { Router } from "express";
-import { checkBackUpShellController, shellController1 } from "../controllers/shellController";
-import { logsViewController, logsErrTestController, rateLimitLogsViewController } from "../controllers/logController";
+import shellRouter from "./shellRouter";
 
 const webRouter: Router = express.Router();
+webRouter.use("/shell", shellRouter);
 
 webRouter.get("/ping", (req, res) => {
     console.log("Request Method", req.method);
     res.status(200).json({message: "Connected to web route."});
 });
-
-webRouter.get("/shell-test", shellController1);
-webRouter.get("/shell-backup-check", checkBackUpShellController);
-webRouter.get("/logs-view", logsViewController);
-webRouter.get("/rate-limit-logs-view", rateLimitLogsViewController);
-webRouter.get("/logs-err-test", logsErrTestController);
 
 export default webRouter;
